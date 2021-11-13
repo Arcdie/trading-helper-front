f96ed4429bfb4a0b0bf915e03765878bf6482653
Valentyn <xdshkaa2@gmail.com>
2021-11-13 13:49:22 +0200

    master: fixed bugs in robots-statistics

--- public/javascripts/web/experiment.js
+++ public/javascripts/web/experiment.js
@@ -8,6 +8,7 @@ objects, windows, moment, user, wsClient, ChartCandles, ChartVolume, Lightweight
 const URL_GET_TRADES = '/api/trades';
 const URL_GET_CANDLES = '/api/candles';
 const URL_GET_ACTIVE_INSTRUMENTS = '/api/instruments/active';
+const URL_GET_INSTRUMENT_ROBOT_BOUNDS = '/api/instrument-robot-bounds';
 
 const AVAILABLE_PERIODS = new Map([
   ['1M', '1m'],
@@ -31,6 +32,7 @@ const windowHeight = window.innerHeight;
 let LIMITER_FOR_AGGREGATE_TRADES = 3;
 const DEFAULT_PERIOD = AVAILABLE_PERIODS.get('1M');
 
+let isLoading = false;
 let choosenInstrumentId;
 let choosenPeriod = DEFAULT_PERIOD;
 
@@ -38,6 +40,7 @@ let chartVolume = {};
 let chartCandles = {};
 
 let robots = [];
+let trades = [];
 let instrumentsDocs = [];
 let instrumentRobotBounds = [];
 
@@ -46,6 +49,8 @@ const $rootContainer = $('.charts');
 const $chartPeriods = $('.chart-periods div');
 const $instrumentsContainer = $('.instruments-container');
 const $instrumentsList = $instrumentsContainer.find('.instruments-list .list');
+
+const $loader = $('.loader');
 
 const $legend = $('.legend');
 const $low = $legend.find('span.low');
@@ -56,6 +61,8 @@ const $percent = $legend.find('span.percent');
 
 const $tradesSlider = $('.trades-slider');
 const $tradesAggregate = $('.trades-aggregate input');
+
+const $robotsContainer = $('.robots-container');
 
 $(document).ready(async () => {
   $rootContainer.css({ height: windowHeight - 20 });
@@ -92,6 +99,10 @@ $(document).ready(async () => {
 
   $chartPeriods
     .on('click', async function () {
+      if (isLoading) {
+        return true;
+      }
+
       const $period = $(this);
       const period = $period.data('period');
 
@@ -101,11 +112,17 @@ $(document).ready(async () => {
       choosenPeriod = period;
 
       if (choosenInstrumentId) {
+        isLoading = true;
+        $loader.addClass('is_active');
+
         await loadChart({
           instrumentId: choosenInstrumentId,
         });
 
-        loadMarkers(instrumentRobotBounds);
+        loadMarkers(trades);
+        fillRobotsInfo(instrumentRobotBounds, robots);
+
+        $loader.removeClass('is_active');
       }
     });
 
@@ -134,6 +151,13 @@ $(document).ready(async () => {
         return true;
       }
 
+      if (isLoading) {
+        return true;
+      }
+
+      isLoading = true;
+      $loader.addClass('is_active');
+
       $instrumentsList
         .find('.instrument')
         .removeClass('is_active');
@@ -143,30 +167,55 @@ $(document).ready(async () => {
 
       choosenInstrumentId = instrumentId;
 
+      const resultGetInstrumentRobotBounds = await makeRequest({
+        method: 'GET',
+        url: `${URL_GET_INSTRUMENT_ROBOT_BOUNDS}?instrumentId=${instrumentId}`,
+      });
+
+      if (!resultGetInstrumentRobotBounds || !resultGetInstrumentRobotBounds.status) {
+        isLoading = false;
+        $loader.removeClass('is_active');
+        alert(resultGetInstrumentRobotBounds.message || 'Cant makeRequest URL_GET_INSTRUMENT_ROBOT_BOUNDS');
+        return true;
+      }
+
       const resultGetTrades = await makeRequest({
         method: 'GET',
         url: `${URL_GET_TRADES}?instrumentId=${instrumentId}`,
       });
 
       if (!resultGetTrades || !resultGetTrades.status) {
+        isLoading = false;
+        $loader.removeClass('is_active');
         alert(resultGetInstruments.message || 'Cant makeRequest URL_GET_TRADES');
         return true;
       }
 
       if (choosenInstrumentId !== instrumentId) {
+        isLoading = false;
+        $loader.removeClass('is_active');
         return true;
       }
 
-      instrumentRobotBounds = resultGetTrades.result;
+      trades = resultGetTrades.result;
+      instrumentRobotBounds = resultGetInstrumentRobotBounds.result;
 
       await loadChart({ instrumentId });
-      loadMarkers(instrumentRobotBounds);
+      loadMarkers(trades);
+      fillRobotsInfo(instrumentRobotBounds, robots);
     });
 
   $tradesSlider
     .find('button')
     .on('click', function () {
       if (!choosenInstrumentId) {
+        return true;
+      }
+
+      const $amountTicks = $tradesSlider.find('span.amount-ticks');
+      const amountTicks = parseInt($amountTicks.text(), 10);
+
+      if (amountTicks === 0) {
         return true;
       }
 
@@ -183,9 +232,20 @@ $(document).ready(async () => {
 
         if (choosenInstrumentId) {
           $tradesSlider.find('span.current-tick').text(0);
-          loadMarkers(instrumentRobotBounds);
+          loadMarkers(trades);
+          fillRobotsInfo(instrumentRobotBounds, robots);
         }
       }
+    });
+
+  $robotsContainer
+    .on('change', '.checkbox', function () {
+      const boundId = $(this).closest('tr').data('boundid');
+
+      const targetBound = instrumentRobotBounds.find(bound => bound._id === boundId);
+      targetBound.is_active = this.checked;
+
+      loadMarkers(trades);
     });
 });
 
@@ -202,6 +262,8 @@ const loadChart = async ({
   });
 
   if (!resultGetCandles || !resultGetCandles.status) {
+    isLoading = false;
+    $loader.removeClass('is_active');
     alert(resultGetCandles.message || `Cant makeRequest ${URL_GET_CANDLES}`);
     return true;
   }
@@ -214,6 +276,8 @@ const loadChart = async ({
   $rootContainer.empty();
 
   if (!resultGetCandles.result || !resultGetCandles.result.length) {
+    isLoading = false;
+    $loader.removeClass('is_active');
     return true;
   }
 
@@ -302,6 +366,9 @@ const loadChart = async ({
       });
     });
   });
+
+  isLoading = false;
+  $loader.removeClass('is_active');
 };
 
 const loadMarkers = (trades) => {
@@ -310,27 +377,34 @@ const loadMarkers = (trades) => {
   chartCandles.removeMarkers();
 
   const divider = choosenPeriod === AVAILABLE_PERIODS.get('1M') ? 60 : 300;
+  const startTimeFirstCandle = chartCandles.originalData[0].originalTimeUnix;
 
-  trades.forEach(trade => {
-    const timeUnix = moment(trade.time).utc().unix();
-    const targetInterval = timeUnix - (timeUnix % divider);
+  trades
+    .forEach(trade => {
+      const timeUnix = moment(trade.time).utc().unix();
 
-    const keyMarker = `i${targetInterval}`;
-    const direction = trade.is_long ? 'long' : 'short';
+      if (timeUnix < startTimeFirstCandle) {
+        return true;
+      }
 
-    if (!newMarkers[keyMarker]) {
-      newMarkers[keyMarker] = {};
-    }
+      const targetInterval = timeUnix - (timeUnix % divider);
 
-    if (!newMarkers[keyMarker][`q${trade.quantity}-${direction}`]) {
-      newMarkers[keyMarker][`q${trade.quantity}-${direction}`] = [];
-    }
+      const keyMarker = `i${targetInterval}`;
+      const direction = trade.is_long ? 'long' : 'short';
 
-    newMarkers[keyMarker][`q${trade.quantity}-${direction}`].push({
-      ...trade,
-      targetInterval,
+      if (!newMarkers[keyMarker]) {
+        newMarkers[keyMarker] = {};
+      }
+
+      if (!newMarkers[keyMarker][`q${trade.quantity}-${direction}`]) {
+        newMarkers[keyMarker][`q${trade.quantity}-${direction}`] = [];
+      }
+
+      newMarkers[keyMarker][`q${trade.quantity}-${direction}`].push({
+        ...trade,
+        targetInterval,
+      });
     });
-  });
 
   Object.keys(newMarkers).forEach(keyInterval => {
     Object.keys(newMarkers[keyInterval]).forEach(keyQuantity => {
@@ -339,6 +413,13 @@ const loadMarkers = (trades) => {
       if (workTrades.length < LIMITER_FOR_AGGREGATE_TRADES) {
         return true;
       }
+
+      const quantity = workTrades[0].quantity;
+      const isLong = workTrades[0].is_long;
+
+      const targetBound = instrumentRobotBounds.find(
+        bound => bound.quantity === quantity && bound.is_long === isLong,
+      );
 
       let color, shape;
 
@@ -350,21 +431,28 @@ const loadMarkers = (trades) => {
         shape = 'arrowDown';
       }
 
+      const lTrades = workTrades.length;
+
       robots.push({
         shape,
         color,
         time: workTrades[0].targetInterval,
-        text: `${workTrades[0].quantity}: ${workTrades.length}`,
+        text: `${workTrades[0].quantity}: ${lTrades}`,
+        quantity,
+        is_long: isLong,
+        is_active: targetBound.is_active,
       });
     });
   });
 
+  const activeRobots = robots.filter(robot => robot.is_active);
+
   $tradesSlider
     .find('span.amount-ticks')
-    .text(robots.length);
+    .text(activeRobots.length);
 
-  if (robots.length) {
-    robots
+  if (activeRobots.length) {
+    activeRobots
       .sort((a, b) => a.time < b.time ? -1 : 1)
       .forEach(newMarker => {
         chartCandles.addMarker(newMarker);
@@ -426,4 +514,31 @@ const scrollTo = (action) => {
   chartCandles.chart
     .timeScale()
     .scrollToPosition(-candlesToTargetRobot, false);
+};
+
+const fillRobotsInfo = (bounds, robots) => {
+  let appendStr = `<table>
+    <tr>
+      <th>Название</th>
+      <th>К-во</th>
+      <th>Показать</th>
+    </tr>
+  `;
+
+  bounds
+    .sort((a, b) => a.quantity > b.quantity ? -1 : 1)
+    .forEach(bound => {
+      const numberRobots = robots.filter(
+        robot => robot.quantity === bound.quantity && robot.is_long === bound.is_long,
+      ).length;
+
+      appendStr += `<tr data-boundid=${bound._id}>
+        <td>${bound.quantity} ${bound.is_long ? 'long' : 'short'}</td>
+        <td>${numberRobots}</td>
+        <td><input class="checkbox" type="checkbox" ${bound.is_active ? 'checked' : ''}></td>
+      </tr>`;
+    });
+
+  appendStr += '</table>';
+  $robotsContainer.empty().append(appendStr);
 };
--- public/javascripts/web/robots-statistics.js
+++ public/javascripts/web/robots-statistics.js
@@ -1,46 +1,544 @@
-/* global makeRequest */
+/* global
+functions, makeRequest, initPopWindow,
+objects, windows, moment, user, wsClient, ChartCandles, ChartVolume, LightweightCharts
+*/
 
 /* Constants */
 
-const URL_GET_INSTRUMENTS_WITH_ROBOTS = '/api/instruments/by-robots';
+const URL_GET_TRADES = '/api/trades';
+const URL_GET_CANDLES = '/api/candles';
+const URL_GET_ACTIVE_INSTRUMENTS = '/api/instruments/active';
+const URL_GET_INSTRUMENT_ROBOT_BOUNDS = '/api/instrument-robot-bounds';
 
+const AVAILABLE_PERIODS = new Map([
+  ['1M', '1m'],
+  ['5M', '5m'],
+  ['1H', '1h'],
+  ['4H', '4h'],
+  ['1D', '1d'],
+]);
+
+const WORKING_PERIODS = [
+  AVAILABLE_PERIODS.get('1M'),
+  AVAILABLE_PERIODS.get('5M'),
+  // AVAILABLE_PERIODS.get('1H'),
+  // AVAILABLE_PERIODS.get('4H'),
+  // AVAILABLE_PERIODS.get('1D'),
+];
+
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+
+let LIMITER_FOR_AGGREGATE_TRADES = 3;
+const DEFAULT_PERIOD = AVAILABLE_PERIODS.get('1M');
+
+let isLoading = false;
+let choosenInstrumentId;
+let choosenPeriod = DEFAULT_PERIOD;
+
+let chartVolume = {};
+let chartCandles = {};
+
+let robots = [];
+let trades = [];
+let instrumentsDocs = [];
+let instrumentRobotBounds = [];
 
 /* JQuery */
-const $container = $('.container-instruments');
+const $rootContainer = $('.charts');
+const $chartPeriods = $('.chart-periods div');
+const $instrumentsContainer = $('.instruments-container');
+const $instrumentsList = $instrumentsContainer.find('.instruments-list .list');
 
-/* Functions */
+const $loader = $('.loader');
+
+const $legend = $('.legend');
+const $low = $legend.find('span.low');
+const $high = $legend.find('span.high');
+const $open = $legend.find('span.open');
+const $close = $legend.find('span.close');
+const $percent = $legend.find('span.percent');
+
+const $tradesSlider = $('.trades-slider');
+const $tradesAggregate = $('.trades-aggregate input');
+
+const $robotsContainer = $('.robots-container');
 
 $(document).ready(async () => {
+  $rootContainer.css({ height: windowHeight - 20 });
+
   const resultGetInstruments = await makeRequest({
     method: 'GET',
-    url: URL_GET_INSTRUMENTS_WITH_ROBOTS,
+    url: `${URL_GET_ACTIVE_INSTRUMENTS}?doesExistRobot=true`,
   });
 
-  if (resultGetInstruments && resultGetInstruments.status) {
-    let appendStr = '';
+  if (!resultGetInstruments || !resultGetInstruments.status) {
+    alert(resultGetInstruments.message || 'Cant makeRequest URL_GET_ACTIVE_INSTRUMENTS');
+    return true;
+  }
 
-    resultGetInstruments.result.forEach(instrument => {
-      instrument.tick_sizes_for_robot.forEach(tick => {
-        appendStr += `<a
-          class="instrument"
-          data-tickid="${tick._id}"
-          data-instrumentid="${instrument._id}"
-          href="/instrument-tick-bounds?instrumentId=${instrument._id}&tickId=${tick._id}"
-        >
-          <span class="instrument-name">${instrument.name_spot}</span>
-          <p><span>Объем: ${tick.value}</span><span>Направление: ${tick.direction}</span></p>
-        </a>`;
+  instrumentsDocs = resultGetInstruments.result;
+
+  $instrumentsContainer
+    .css({ maxHeight: windowHeight });
+
+  $instrumentsList
+    .css({ maxHeight: windowHeight });
+
+  renderListInstruments(instrumentsDocs);
+
+  WORKING_PERIODS.forEach(period => {
+    const $period = $chartPeriods.parent().find(`.${period}`);
+
+    $period.addClass('is_worked');
+
+    if (period === DEFAULT_PERIOD) {
+      $period.addClass('is_active');
+    }
+  });
+
+  $chartPeriods
+    .on('click', async function () {
+      if (isLoading) {
+        return true;
+      }
+
+      const $period = $(this);
+      const period = $period.data('period');
+
+      $chartPeriods.removeClass('is_active');
+      $period.addClass('is_active');
+
+      choosenPeriod = period;
+
+      if (choosenInstrumentId) {
+        isLoading = true;
+        $loader.addClass('is_active');
+
+        await loadChart({
+          instrumentId: choosenInstrumentId,
+        });
+
+        loadMarkers(trades);
+        fillRobotsInfo(instrumentRobotBounds, robots);
+
+        $loader.removeClass('is_active');
+      }
+    });
+
+  $('.search input')
+    .on('keyup', function () {
+      const value = $(this).val().toLowerCase();
+
+      let targetDocs = instrumentsDocs;
+
+      if (value) {
+        targetDocs = targetDocs.filter(doc => doc.name
+          .toLowerCase()
+          .includes(value),
+        );
+      }
+
+      renderListInstruments(targetDocs);
+    });
+
+  $instrumentsList
+    .on('click', '.instrument', async function () {
+      const $instrument = $(this);
+      const instrumentId = $instrument.data('instrumentid');
+
+      if (choosenInstrumentId === instrumentId) {
+        return true;
+      }
+
+      if (isLoading) {
+        return true;
+      }
+
+      isLoading = true;
+      $loader.addClass('is_active');
+
+      $instrumentsList
+        .find('.instrument')
+        .removeClass('is_active');
+
+      $instrument.addClass('is_active');
+      $tradesSlider.find('span.current-tick').text(0);
+
+      choosenInstrumentId = instrumentId;
+
+      const resultGetInstrumentRobotBounds = await makeRequest({
+        method: 'GET',
+        url: `${URL_GET_INSTRUMENT_ROBOT_BOUNDS}?instrumentId=${instrumentId}`,
+      });
+
+      if (!resultGetInstrumentRobotBounds || !resultGetInstrumentRobotBounds.status) {
+        isLoading = false;
+        $loader.removeClass('is_active');
+        alert(resultGetInstrumentRobotBounds.message || 'Cant makeRequest URL_GET_INSTRUMENT_ROBOT_BOUNDS');
+        return true;
+      }
+
+      const resultGetTrades = await makeRequest({
+        method: 'GET',
+        url: `${URL_GET_TRADES}?instrumentId=${instrumentId}`,
+      });
+
+      if (!resultGetTrades || !resultGetTrades.status) {
+        isLoading = false;
+        $loader.removeClass('is_active');
+        alert(resultGetInstruments.message || 'Cant makeRequest URL_GET_TRADES');
+        return true;
+      }
+
+      if (choosenInstrumentId !== instrumentId) {
+        isLoading = false;
+        $loader.removeClass('is_active');
+        return true;
+      }
+
+      trades = resultGetTrades.result;
+      instrumentRobotBounds = resultGetInstrumentRobotBounds.result;
+
+      await loadChart({ instrumentId });
+      loadMarkers(trades);
+      fillRobotsInfo(instrumentRobotBounds, robots);
+    });
+
+  $tradesSlider
+    .find('button')
+    .on('click', function () {
+      if (!choosenInstrumentId) {
+        return true;
+      }
+
+      const $amountTicks = $tradesSlider.find('span.amount-ticks');
+      const amountTicks = parseInt($amountTicks.text(), 10);
+
+      if (amountTicks === 0) {
+        return true;
+      }
+
+      scrollTo($(this).attr('class'));
+    });
+
+  $tradesAggregate
+    .on('keyup', function () {
+      const newValue = parseInt($(this).val(), 10);
+
+      if (!Number.isNaN(newValue)
+        && newValue !== LIMITER_FOR_AGGREGATE_TRADES) {
+        LIMITER_FOR_AGGREGATE_TRADES = newValue;
+
+        if (choosenInstrumentId) {
+          $tradesSlider.find('span.current-tick').text(0);
+          loadMarkers(trades);
+          fillRobotsInfo(instrumentRobotBounds, robots);
+        }
+      }
+    });
+
+  $robotsContainer
+    .on('change', '.checkbox', function () {
+      const boundId = $(this).closest('tr').data('boundid');
+
+      const targetBound = instrumentRobotBounds.find(bound => bound._id === boundId);
+      targetBound.is_active = this.checked;
+
+      loadMarkers(trades);
+    });
+});
+
+const loadChart = async ({
+  instrumentId,
+}) => {
+  console.log('start loading');
+
+  const endTime = new Date().toISOString();
+
+  const resultGetCandles = await makeRequest({
+    method: 'GET',
+    url: `${URL_GET_CANDLES}/${choosenPeriod}?instrumentId=${instrumentId}&&endTime=${endTime}`,
+  });
+
+  if (!resultGetCandles || !resultGetCandles.status) {
+    isLoading = false;
+    $loader.removeClass('is_active');
+    alert(resultGetCandles.message || `Cant makeRequest ${URL_GET_CANDLES}`);
+    return true;
+  }
+
+  console.log('end loading');
+
+  chartCandles = {};
+  chartVolume = {};
+
+  $rootContainer.empty();
+
+  if (!resultGetCandles.result || !resultGetCandles.result.length) {
+    isLoading = false;
+    $loader.removeClass('is_active');
+    return true;
+  }
+
+  choosenInstrumentId = instrumentId;
+  const targetDoc = instrumentsDocs.find(doc => doc._id === instrumentId);
+
+  chartCandles = new ChartCandles($rootContainer, choosenPeriod, targetDoc);
+  chartVolume = new ChartVolume($rootContainer);
+
+  chartCandles.setOriginalData(resultGetCandles.result, false);
+
+  const listCharts = [chartCandles, chartVolume];
+
+  chartCandles.drawSeries(chartCandles.mainSeries, chartCandles.originalData);
+  chartVolume.drawSeries(chartCandles.originalData);
+
+  if (['1m', '5m'].includes(choosenPeriod)) {
+    listCharts.forEach(chartWrapper => {
+      chartWrapper.chart.applyOptions({
+        timeScale: {
+          timeVisible: true,
+        },
+      });
+    });
+  } else {
+    listCharts.forEach(chartWrapper => {
+      chartWrapper.chart.applyOptions({
+        timeScale: {
+          timeVisible: false,
+        },
+      });
+    });
+  }
+
+  chartCandles.chart.subscribeCrosshairMove((param) => {
+    if (param.time) {
+      const price = param.seriesPrices.get(chartCandles.mainSeries);
+
+      if (price) {
+        const differenceBetweenHighAndLow = price.high - price.low;
+        const percentPerPrice = 100 / (price.open / differenceBetweenHighAndLow);
+
+        $open.text(price.open);
+        $close.text(price.close);
+        $low.text(price.low);
+        $high.text(price.high);
+        $percent.text(`${percentPerPrice.toFixed(1)}%`);
+      }
+    }
+  });
+
+  chartCandles.chart.subscribeClick((param) => {
+    if (param.time) {
+      const indexRobots = robots.findIndex(robot => robot.time === param.time);
+
+      if (~indexRobots) {
+        $tradesSlider
+          .find('span.current-tick')
+          .text(indexRobots + 1);
+      }
+    }
+  });
+
+  let isCrossHairMoving = false;
+
+  listCharts.forEach(elem => {
+    const otherCharts = listCharts.filter(chart => chart.containerName !== elem.containerName);
+
+    elem.chart.subscribeCrosshairMove(param => {
+      if (!param.point || !param.time || isCrossHairMoving) {
+        return true;
+      }
+
+      isCrossHairMoving = true;
+
+      otherCharts.forEach(innerElem => {
+        innerElem.chart.moveCrosshair(param.point);
+      });
+
+      isCrossHairMoving = false;
+
+      elem.chart.timeScale().subscribeVisibleLogicalRangeChange(range => {
+        otherCharts.forEach(innerElem => {
+          innerElem.chart.timeScale().setVisibleLogicalRange(range);
+        });
+      });
+    });
+  });
+
+  isLoading = false;
+  $loader.removeClass('is_active');
+};
+
+const loadMarkers = (trades) => {
+  robots = [];
+  const newMarkers = [];
+  chartCandles.removeMarkers();
+
+  const divider = choosenPeriod === AVAILABLE_PERIODS.get('1M') ? 60 : 300;
+  const startTimeFirstCandle = chartCandles.originalData[0].originalTimeUnix;
+
+  trades
+    .forEach(trade => {
+      const timeUnix = moment(trade.time).utc().unix();
+
+      if (timeUnix < startTimeFirstCandle) {
+        return true;
+      }
+
+      const targetInterval = timeUnix - (timeUnix % divider);
+
+      const keyMarker = `i${targetInterval}`;
+      const direction = trade.is_long ? 'long' : 'short';
+
+      if (!newMarkers[keyMarker]) {
+        newMarkers[keyMarker] = {};
+      }
+
+      if (!newMarkers[keyMarker][`q${trade.quantity}-${direction}`]) {
+        newMarkers[keyMarker][`q${trade.quantity}-${direction}`] = [];
+      }
+
+      newMarkers[keyMarker][`q${trade.quantity}-${direction}`].push({
+        ...trade,
+        targetInterval,
       });
     });
 
-    $container.append(appendStr);
+  Object.keys(newMarkers).forEach(keyInterval => {
+    Object.keys(newMarkers[keyInterval]).forEach(keyQuantity => {
+      const workTrades = newMarkers[keyInterval][keyQuantity];
+
+      if (workTrades.length < LIMITER_FOR_AGGREGATE_TRADES) {
+        return true;
+      }
+
+      const quantity = workTrades[0].quantity;
+      const isLong = workTrades[0].is_long;
+
+      const targetBound = instrumentRobotBounds.find(
+        bound => bound.quantity === quantity && bound.is_long === isLong,
+      );
+
+      let color, shape;
+
+      if (workTrades[0].is_long) {
+        color = '#4CAF50';
+        shape = 'arrowUp';
+      } else {
+        color = '#FF5252';
+        shape = 'arrowDown';
+      }
+
+      const lTrades = workTrades.length;
+
+      robots.push({
+        shape,
+        color,
+        time: workTrades[0].targetInterval,
+        text: `${workTrades[0].quantity}: ${lTrades}`,
+        quantity,
+        is_long: isLong,
+        is_active: targetBound.is_active,
+      });
+    });
+  });
+
+  const activeRobots = robots.filter(robot => robot.is_active);
+
+  $tradesSlider
+    .find('span.amount-ticks')
+    .text(activeRobots.length);
+
+  if (activeRobots.length) {
+    activeRobots
+      .sort((a, b) => a.time < b.time ? -1 : 1)
+      .forEach(newMarker => {
+        chartCandles.addMarker(newMarker);
+      });
+
+    chartCandles.drawMarkers();
+    scrollTo('next');
+  }
+};
+
+const renderListInstruments = targetDocs => {
+  let appendInstrumentsStr = '';
+
+  targetDocs.forEach(doc => {
+    appendInstrumentsStr += `<div
+      id="instrument-${doc._id}"
+      class="instrument"
+      data-instrumentid=${doc._id}>
+      <span class="instrument-name">${doc.name}</span>
+    </div>`;
+  });
+
+  $instrumentsList
+    .empty()
+    .append(appendInstrumentsStr);
+};
+
+const scrollTo = (action) => {
+  const $currentTick = $tradesSlider.find('span.current-tick');
+
+  let currentTick = parseInt($currentTick.text(), 10);
+  const amountTicks = parseInt($tradesSlider.find('span.amount-ticks').text(), 10);
+
+  if (action === 'next') {
+    currentTick += 1;
+  } else {
+    currentTick -= 1;
   }
 
-  $container
-    .on('click', '.instrument', function () {
-      const $instrument = $(this);
+  if (currentTick === 0) {
+    currentTick = amountTicks;
+  }
 
-      const tickId = $instrument.data('tickid');
-      const instrumentId = $instrument.data('instrumentid');
+  if (currentTick === amountTicks + 1) {
+    currentTick = 1;
+  }
+
+  $currentTick.text(currentTick);
+
+  let candlesToTargetRobot = 0;
+  const targetRobot = robots[currentTick - 1];
+
+  for (let i = chartCandles.originalData.length - 1; i >= 0; i -= 1) {
+    if (chartCandles.originalData[i].originalTimeUnix === targetRobot.time) {
+      candlesToTargetRobot = chartCandles.originalData.length - i; break;
+    }
+  }
+
+  chartCandles.chart
+    .timeScale()
+    .scrollToPosition(-candlesToTargetRobot, false);
+};
+
+const fillRobotsInfo = (bounds, robots) => {
+  let appendStr = `<table>
+    <tr>
+      <th>Название</th>
+      <th>К-во</th>
+      <th>Показать</th>
+    </tr>
+  `;
+
+  bounds
+    .sort((a, b) => a.quantity > b.quantity ? -1 : 1)
+    .forEach(bound => {
+      const numberRobots = robots.filter(
+        robot => robot.quantity === bound.quantity && robot.is_long === bound.is_long,
+      ).length;
+
+      appendStr += `<tr data-boundid=${bound._id}>
+        <td>${bound.quantity} ${bound.is_long ? 'long' : 'short'}</td>
+        <td>${numberRobots}</td>
+        <td><input class="checkbox" type="checkbox" ${bound.is_active ? 'checked' : ''}></td>
+      </tr>`;
     });
-});
+
+  appendStr += '</table>';
+  $robotsContainer.empty().append(appendStr);
+};
