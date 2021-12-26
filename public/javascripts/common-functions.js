2c23fd48f9ab1c256194d631dd4b5181a14000ab
Valentyn <xdshkaa2@gmail.com>
2021-12-26 16:06:18 +0200

    master: added new pages

--- public/javascripts/common-functions.js
+++ public/javascripts/common-functions.js
@@ -33,6 +33,33 @@ const getRandomNumber = (min, max) => {
 const getUnix = targetDate =>
   parseInt((targetDate ? new Date(targetDate) : new Date()).getTime() / 1000, 10);
 
+const getQueue = (arr, limiter) => {
+  const queues = [];
+  const lArr = arr.length;
+
+  let targetIndex = 0;
+  const numberIterations = Math.ceil(lArr / limiter);
+
+  for (let i = 0; i < numberIterations; i += 1) {
+    const newQueue = [];
+
+    let conditionValue = limiter;
+
+    if (i === (numberIterations - 1)) {
+      conditionValue = lArr - targetIndex;
+    }
+
+    for (let j = 0; j < conditionValue; j += 1) {
+      newQueue.push(arr[targetIndex]);
+      targetIndex += 1;
+    }
+
+    queues.push(newQueue);
+  }
+
+  return queues;
+};
+
 const initPopWindow = (str) => {
   $modalWindow
     .find('div.md-content')
